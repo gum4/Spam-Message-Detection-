53780816e9e39e371f26178c1931a0b9ee7bcf2f
NeverThinkOfLWJ <46687992+gum4@users.noreply.github.com>
2020-07-23 14:47:25 -0400

    neural network version

--- spam.py
+++ spam.py
@@ -17,12 +17,23 @@ from sklearn.datasets.samples_generator import make_blobs
 import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
 from sklearn.naive_bayes import GaussianNB
+import torch
+import torch.nn.functional as F
+from torch.autograd import Variable
+
+
+
+
+
+
+
+
 tag_train=[]
 sentences_train=[]
 with open('train.csv', 'r') as file:
     reader = csv.reader(file)
     for row in reader:
-        tag_train.append(1 if row[0]=='spam' else 0)
+        tag_train.append([1] if row[0]=='spam' else [0])
         item=row[1].split(' ')
         tmp=""
         for i in item:
@@ -42,13 +53,24 @@ word = vectorizer.get_feature_names()
 
 weight = X.toarray()
 
-pca = PCA(n_components=2)
+pca = PCA(n_components=10)
 pca.fit(weight)
 
 W=pca.transform(weight)
 
-clf = GaussianNB()
-clf.fit(W,tag_train)
+WW=torch.from_numpy(W).float()
+
+TT=torch.from_numpy(np.array(tag_train)).float()
+
+WW,TT=Variable(WW),Variable (TT)
+
+
+
+
+
+
+#clf = GaussianNB()
+#clf.fit(W,tag_train)
 #print ("==Predict result by predict==")
 #print(clf.predict([[-0.8, -1]]))
 #print ("==Predict result by predict_proba==")
@@ -81,11 +103,74 @@ word1 = vectorizer1.get_feature_names()
 weight1 = X1.toarray()
 pca.fit(weight1)
 
+
 W1=pca.transform(weight1)
-for i in range(0,len(W1)):
-    predictions.append('spam' if clf.predict([W1[i]])[0]==1 else 'ham')
+WW1=torch.from_numpy(W1).float()
+
+#for i in range(0,len(W1)):
+#    predictions.append('spam' if clf.predict([W1[i]])[0]==1 else 'ham')
 
 
-res = pd.DataFrame({'SmsId':indexes,'Label':predictions})
-res.to_csv("sampleSubmission.csv",index=False,sep=',')
+#res = pd.DataFrame({'SmsId':indexes,'Label':predictions})
+#res.to_csv("sampleSubmission.csv",index=False,sep=',')
+
+
+
+
+
+#####
+class TwoLayerNet(torch.nn.Module):
+    def __init__(self, D_in, H, D_out):
+        """
+        In the constructor we instantiate two nn.Linear modules and assign them as
+        member variables.
+        """
+        super(TwoLayerNet, self).__init__()
+        self.linear1 = torch.nn.Linear(D_in, H)
+        self.linear2 = torch.nn.Linear(H, D_out)
+
+    def forward(self, x):
+        """
+        In the forward function we accept a Tensor of input data and we must return
+        a Tensor of output data. We can use Modules defined in the constructor as
+        well as arbitrary operators on Tensors.
+        """
+        h_relu = self.linear1(x).clamp(min=0)
+        y_pred = self.linear2(h_relu)
+        return y_pred
+
+
+# N is batch size; D_in is input dimension;
+# H is hidden dimension; D_out is output dimension.
+N, D_in, H, D_out = 5572, 10, 10, 1
+
+# Create random Tensors to hold inputs and outputs
+x = torch.randn(N, D_in)
+y = torch.randn(N, D_out)
+
+
+# Construct our model by instantiating the class defined above
+model = TwoLayerNet(D_in, H, D_out)
+
+# Construct our loss function and an Optimizer. The call to model.parameters()
+# in the SGD constructor will contain the learnable parameters of the two
+# nn.Linear modules which are members of the model.
+criterion = torch.nn.MSELoss(reduction='sum')
+optimizer = torch.optim.SGD(model.parameters(), lr=1e-4)
+for t in range(500):
+    # Forward pass: Compute predicted y by passing x to the model
+    y_pred = model(WW)
+
+    # Compute and print loss
+    loss = criterion(y_pred, TT)
+    #if t % 100 == 99:
+    #    print(t, loss.item())
+
+    # Zero gradients, perform a backward pass, and update the weights.
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+    
+
+model(WW1)
 
